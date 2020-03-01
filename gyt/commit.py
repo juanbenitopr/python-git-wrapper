34633d9de6e447c3a4ebf9ab0fb57cdf7bbfe2a8
Juan Benito <juanbenito@citibox.com>
2020-03-01 14:47:20 +0100

    feat (commit): improved commit support

---
+++ gyt/commit.py
@@ -0,0 +1,24 @@
+import datetime
+
+
+class Commit:
+
+    def __init__(self, hash: str, message: str, date_time: datetime.datetime, author: str):
+        self._hash = hash
+        self.message = message
+        self.datetime = date_time
+        self.author = author
+
+    @property
+    def hash(self):
+        return self._hash
+
+    @hash.setter
+    def hash(self, value):
+        raise Exception('There is no possible set a new hash value, instance a new commit instead')
+
+    def __hash__(self) -> str:
+        return self._hash
+
+    def __eq__(self, other: 'Commit') -> bool:
+        return self.hash == other.hash
