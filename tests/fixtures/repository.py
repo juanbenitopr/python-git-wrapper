9c9d17fedea934e15de9dce76c8136be2a2a7a9c
Juan Benito <juanbenito@citibox.com>
2020-03-01 23:37:03 +0100

    feat (branch): improved support for branches

--- tests/fixtures/repository.py
+++ tests/fixtures/repository.py
@@ -1,9 +1,11 @@
 import os
 import shutil
+from random import randint
 
 import pytest
 
 from gyt import Repository
+from gyt.commit import Commit
 
 
 @pytest.fixture
@@ -21,13 +23,12 @@ def empty_repository() -> Repository:
 @pytest.fixture
 def repository_with_file() -> Repository:
     from uuid import uuid4
-    from random import randint
 
     directory_name = str(uuid4())
-    file_name = str(randint(0, 100000))
-
     os.mkdir(directory_name)
-    open(os.path.join(directory_name, file_name), 'w').close()
+
+    file_name = create_random_file_in_directory(directory_name)
+
     repository = Repository.build(directory_name, create_repository=True)
 
     yield repository, file_name
@@ -38,7 +39,6 @@ def repository_with_file() -> Repository:
 @pytest.fixture
 def repository_with_commits() -> Repository:
     from uuid import uuid4
-    from random import randint
 
     directory_name = str(uuid4())
     file_name = str(randint(0, 100000))
@@ -52,3 +52,19 @@ def repository_with_commits() -> Repository:
     yield repository
 
     shutil.rmtree(directory_name)
+
+
+def create_random_file_in_directory(directory_name: str) -> str:
+
+    file_name = str(randint(0, 100000))
+
+    open(os.path.join(directory_name, file_name), 'w').close()
+    return file_name
+
+
+def create_random_commit(repository: Repository) -> Commit:
+    file_name = create_random_file_in_directory(repository.path)
+
+    repository.add_files([file_name])
+    repository.commit(file_name + "" + str(randint(0, 100000)))
+    return repository.last_commit
--- tests/repository/test_branches.py
+++ tests/repository/test_branches.py
@@ -2,10 +2,13 @@ from typing import Tuple
 
 import pytest
 
+from gyt import GitError
+from gyt.branch import Branch
+from gyt.commit import Commit
 from gyt.repository import Repository
-from tests.fixtures.repository import repository_with_file
+from tests.fixtures.repository import repository_with_file, repository_with_commits, create_random_commit
 
-pytest.mark.usefixtures(repository_with_file)
+pytest.mark.usefixtures(repository_with_file, repository_with_commits)
 
 
 def test_get_current_branch(repository_with_file: Tuple[Repository, str]):
@@ -38,3 +41,63 @@ def test_get_branches(repository_with_file: Tuple[Repository, str]):
     branches = repository.local_branches
 
     assert branches == ['master', 'test_branch']
+
+
+def test_create_branch(repository_with_commits: Repository):
+    branch_name1 = 'test_branch1'
+    branch_name2 = 'test_branch2'
+
+    branch1 = repository_with_commits.create_branch(branch_name1)
+
+    assert branch1 == branch_name1
+    assert branch1 != repository_with_commits.status().get('branch')
+
+    branch2 = repository_with_commits.create_branch(branch_name2, move_to=True)
+
+    assert branch2 == branch_name2
+    assert branch2 == repository_with_commits.status().get('branch')
+
+
+def test_merge_branches(repository_with_commits: Repository):
+    origin_branch = Branch('master')
+    branch_name2 = 'test_branch2'
+
+    branch2 = repository_with_commits.create_branch(branch_name2, move_to=True)
+    random_commit_list = [create_random_commit(repository_with_commits) for i in range(4)]
+
+    branch_merged = repository_with_commits.merge_branches(origin_branch, branch2)
+
+    assert branch_merged == origin_branch
+
+    for i, random_commit in enumerate(random_commit_list):
+        commit_position = len(random_commit_list) - (i + 1)
+        assert random_commit.message == repository_with_commits.get_commit_by_position(commit_position).message
+
+
+def test_merge_branches_with_squash(repository_with_commits: Repository):
+    origin_branch = Branch('master')
+    branch_name2 = 'test_branch2'
+
+    branch2 = repository_with_commits.create_branch(branch_name2, move_to=True)
+    [create_random_commit(repository_with_commits) for i in range(4)]
+
+    branch_merged = repository_with_commits.merge_branches(origin_branch, branch2, squash=True)
+
+    assert branch_merged == origin_branch
+
+    with pytest.raises(GitError):
+        repository_with_commits.get_commit_by_position(2)
+
+
+def test_merge_branches_with_new_commit(repository_with_commits: Repository):
+    origin_branch = Branch('master')
+    branch_name2 = 'test_branch2'
+
+    branch2 = repository_with_commits.create_branch(branch_name2, move_to=True)
+    [create_random_commit(repository_with_commits) for i in range(4)]
+
+    branch_merged = repository_with_commits.merge_branches(origin_branch, branch2, new_commit=True)
+
+    assert branch_merged == origin_branch
+
+    assert isinstance(repository_with_commits.get_commit_by_position(2), Commit)
